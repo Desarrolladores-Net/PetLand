24999b73e2366e012f334c9573b519f5a928a8d5
Brian Pérez López <brianpl990227@gmail.com>
2023-04-07 23:41:43 -0400

    E

--- 1-FrameworksAndDrivers/UI/petshop-nuxt/nuxt.config.ts
+++ 1-FrameworksAndDrivers/UI/petshop-nuxt/nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
 
+export default defineNuxtConfig({
+    css: [
+        "primevue/resources/themes/md-light-indigo/theme.css",
+        "primevue/resources/primevue.css",
+        "primeicons/primeicons.css"
+    ],
+	build: {
+		transpile: ["primevue"]
+	}
 })
