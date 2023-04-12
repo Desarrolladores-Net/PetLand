ed08f1ed6ff5fc95bb9f5d6dd8ee343c178377cc
Brian Pérez López <brianpl990227@gmail.com>
2023-04-12 19:57:45 -0400

    Formato de codigo en clase address

--- 3-Domain/entity/Address.cs
+++ 3-Domain/entity/Address.cs
@@ -8,7 +8,7 @@ namespace Domain.Entity
     public class Address
     {
         public int id { get; set; }
-         public int petId { get; set; }
+        public int petId { get; set; }
         public string province { get; set; }
         public string municipe { get; set; }
         public string streetName { get; set; }
