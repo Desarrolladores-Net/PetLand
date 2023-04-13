dffb63346a476439347424ef34c61c09ecde1e68
JoseA-Cueto <126445911+JoseA-Cueto@users.noreply.github.com>
2023-04-12 19:48:51 -0800

    UpdateEntity

--- 3-Domain/entity/Address.cs
+++ 3-Domain/entity/Address.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Domain.Entidades
+namespace Domain.Entity
 {
     public class Address
     {
+        public int id { get; set; }
+         public int petId { get; set; }
         public string province { get; set; }
         public string municipe { get; set; }
         public string streetName { get; set; }
-
     }
 }
--- 3-Domain/entity/Pet.cs
+++ 3-Domain/entity/Pet.cs
@@ -1,23 +1,20 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Security.AccessControl;
 using System.Threading.Tasks;
 
-namespace Domain.Entidades
+namespace Domain.Entity
 {
     public class Pet
     {
-        [Key]
+
+        public int id { get; set; }
         public int userId { get; set; }
         public string? fullname { get; set; }
         public int? age { get; set; }
         public Address address { get; set; }
         public string? description { get; set; }
         public bool wasAdopted { get; set; }
-        public DateTime adopcionDate { get; set; }
-
-
+        public DateTime adoptionDate { get; set; }
     }
 }
