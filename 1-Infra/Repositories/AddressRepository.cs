772f09542c116c49ec73278b0141d5f4a25ec971
JoseA-Cueto <126445911+JoseA-Cueto@users.noreply.github.com>
2023-04-19 12:28:35 -0800

    Implementacion Repositorios

---
+++ 1-Infra/Repositories/AddressRepository.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entity;
+using Domain.Repositories;
+using Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Repositories
+{
+    public class AddressRepository : IAddressRepository
+    {
+        private AppDbContext _context;
+
+        public AddressRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Address entity) => await _context.AddAsync(entity);
+
+        public async Task<Address> Delete(int id)
+        {
+            var address = await _context.Address.FindAsync(id);
+            _context.Remove(address!);
+            return address!;
+        }
+
+        public Task<List<Address>> GetAll() => _context.Address.ToListAsync();
+
+        public Task<Address> GetOne(int id) => _context.Address.FindAsync(id).AsTask()!;
+
+        public Task GetOneAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Address entity)
+        {
+            _context.Update(entity);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ 1-Infra/Repositories/UserRepository.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entity;
+using Domain.Repositories;
+using Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(User entity) => await _context.AddAsync(entity);
+
+        public async Task<User> Delete(int id)
+        {
+            var user = await _context.User.FindAsync(id);
+            _context.Remove(user!);
+            return user!;
+        }
+
+        public Task<List<User>> GetAll() => _context.User.ToListAsync();
+
+        public Task<User> GetOne(int id) => _context.User.FindAsync(id).AsTask()!;
+
+        public Task GetOneAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(User entity)
+        {
+            _context.Update(entity);
+            return Task.CompletedTask;
+        }
+    }
+}
