38a1b9ffba59b22c2cc43c89741016170ea07e95
Brian Pérez López <brianpl990227@gmail.com>
2023-06-04 19:41:56 -0400

    Adicion de role a la respuesta

--- 3-Domain/ResultObject/User/RegisterResult.cs
+++ 3-Domain/ResultObject/User/RegisterResult.cs
@@ -12,5 +12,6 @@ namespace Domain.ResultObject.User
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Token { get; set; }
+        public string Role { get; set; }
     }
 }
